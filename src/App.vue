2f09ce0b3bf52cabdc7726b65604dc4d7651941a
JackBrens <574950259@qq.com>
2024-03-06 22:41:35 +0800

    feat: 新增右下角固定烂

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
     <RouterView />
   </div>
   <bottomFooter />
+  <fixedNav />
 </template>
 <script setup>
 import { RouterView } from 'vue-router'
 import topHeader from '@/components/topHeader'
 import bottomFooter from '@/components/bottomFooter'
+import fixedNav from '@/components/fixedNav'
 </script>
 
 <style scoped></style>
---
+++ src/components/fixedNav/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="fixed-nav">
+    <div class="fixed-nav-item">
+      <img class="icon" :src="requirePath('weixin.png')" alt="" />
+      <p>二维码</p>
+      <div class="position-box">
+        <img :src="requirePath('contact.jpg')" alt="" />
+      </div>
+    </div>
+    <div class="fixed-nav-item">
+      <img class="icon" :src="requirePath('rexian.png')" alt="" />
+      <p>联系我们</p>
+      <div class="position-box contact"><p style="padding: 10px">400-8876516</p></div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { requirePath } from '../../utils/index.js'
+const list = [
+  {
+    label: '二维码',
+    imgPath: 'weixin.png'
+  },
+  {
+    label: '联系我们',
+    imgPath: 'rexian.png'
+  }
+]
+</script>
+<style scoped lang="scss">
+@import '@/assets/style/variable';
+.fixed-nav {
+  position: fixed;
+  width: 60px;
+  right: 10px;
+  bottom: 10%;
+  z-index: 9;
+  background-color: $bg-color;
+  border-radius: 2px;
+  color: #ffffff;
+  font-size: 12px;
+  .fixed-nav-item {
+    border-bottom: 1px solid #eeeeee40;
+    text-align: center;
+    padding: 10px 0;
+    cursor: pointer;
+    position: relative;
+    &:last-of-type {
+      border-bottom: 0;
+    }
+    &:hover {
+      background-color: #9b6526;
+    }
+    .icon {
+      width: 16px;
+    }
+    &:hover .position-box {
+      display: block;
+    }
+    .position-box {
+      position: absolute;
+      bottom: 0;
+      left: -210px;
+      box-shadow: 0 2px 5px #bdbdbd;
+      background-color: #ffffff;
+      display: none;
+      & > img {
+        width: 200px;
+        height: 240px;
+      }
+    }
+    .contact {
+      background-color: $bg-color;
+      left: -143px;
+      font-size: 18px;
+    }
+  }
+}
+</style>
